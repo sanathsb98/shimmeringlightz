87bd622da6c75b1f3a8cb642c9acdd8d8fce050b
sanathsb98 <itssanathsb@gmail.com>
2024-01-15 22:36:51 +0530

    commit

--- src/components/ourinfo/OurInfo.jsx
+++ src/components/ourinfo/OurInfo.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import '../ourinfo/OurInfo.css';
+import demo from '/src/images/lamp.svg'
 
 const OurInfo = () => {
+    const svgStyle = {
+        width: '516px',
+        height: '360px',
+        position: 'relative',
+      };
+    
+      const imageStyle = {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        
+      };
     return (
         <div className='our-info-container'>
             <div className='our-info-contents'>
@@ -10,6 +26,55 @@ const OurInfo = () => {
                     <div className='info-line'></div>
                 </div>
                 <div className='info-description'>Providing uniform, well-balanced illumination. Aspects of the building architecture and the interior design, who believe that illumination is more than just bulbs and circuits. It's about evoking emotions, creating atmosphere, and guiding the eye like a skilled storyteller.</div>
+                <div className='info-products-container'>
+                    <div className='info-products-left'>
+                        <div className='design-product-box'>
+
+                            <div className='design-product-image'>
+
+
+                                <div className='svg-container'>
+                                    <svg className='immo'  viewBox="0 0 516 360" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        {/* Background Shape */}
+                                        <path d="M0 262.78V30C0 13.4315 13.4315 0 30 0H486C502.569 0 516 13.4315 516 30V330C516 346.569 502.569 360 486 360H178.46C161.891 360 148.46 346.569 148.46 330V322.78C148.46 306.212 135.028 292.78 118.46 292.78H30C13.4315 292.78 0 279.349 0 262.78Z" fill="#E2F3FF" />
+
+                                        {/* Background Image Pattern */}
+                                        <defs>
+                                            <pattern id="image" patternUnits="userSpaceOnUse" width="100%" height="100%">
+                                                <image href="/src/images/lamp.svg" x="0" y="0"  width="516" height="360" preserveAspectRatio="xMidYMid slice" />
+                                            </pattern>
+                                        </defs>
+
+                                        {/* Apply Background Image Pattern to the Shape */}
+                                        <path d="M0 262.78V30C0 13.4315 13.4315 0 30 0H486C502.569 0 516 13.4315 516 30V330C516 346.569 502.569 360 486 360H178.46C161.891 360 148.46 346.569 148.46 330V322.78C148.46 306.212 135.028 292.78 118.46 292.78H30C13.4315 292.78 0 279.349 0 262.78Z" fill="url(#image)" />
+
+                                    </svg>
+                                    <div className='design-intro-title'>
+                                      <div>  Design</div>
+                                    </div>
+                                </div>
+                              
+
+                            </div>
+                            <div className='design-product-des'>
+                                Primary guideline for lighting professionals, architects, and engineers, ensuring optimal lighting conditions for occupants while adhering to energy efficiency and sustainability principles.
+                            </div>
+                        </div>
+                    </div>
+                    <div className='info-products-right'>
+
+                        <div className='info-product-item'>
+                            <div className='info-product-title'>Engineering</div>
+                            <div className='info-product-des'>Renowned software for professional lighting design in buildings. Offers accurate lighting calculations, energy analysis, and realistic visualizations.</div>
+                        </div>
+
+                        <div className='info-product-item'>
+                            <div className='info-product-title'>Supply</div>
+                            <div className='info-product-des'>Project stage helps gauge lead times and potential rush options & meeting the desired delivery dates in line with the project requirement & meeting client expectation.</div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
