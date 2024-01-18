740509877b0fd514292b683d4b9dce1b2717107e
sanathsb98 <itssanathsb@gmail.com>
2024-01-18 13:39:29 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ import { Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Lenis from '@studio-freight/lenis'
 
 
 function App() {
+
+  const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
   return (
     <>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,11 +4,23 @@ import OurInfo from '../../components/ourinfo/OurInfo';
 import ProductsWeOffer from '../../components/productsweoffer/ProductsWeOffer';
 import OurPassion from '../../components/ourpassion/OurPassion';
 import Footer from '../../components/footer/Footer';
-import {motion} from 'framer-motion'
+import {motion} from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 
 
 const Home = () => {
+
+  const animation = {
+    initial: { y: '100%' },
+    enter: i => ({ y: '0%', transition: { duration: 0.75, ease: [0.33, 1, 0.68, 1], delay: 0.075 * i } })
+  }
+
+  const { ref, inView } = useInView({
+    threshold: 0.06,
+    triggerOnce: true
+  })
+
   return (
     <>
     <div className='home-container'>
@@ -18,11 +30,13 @@ const Home = () => {
          <div className='home-empty-space'></div>
            <div className='home-content-info'>
 
-         <div className='home-titles'>
-         <div className='home-title'>Brightening Spaces</div>
-            <br className='gaps'/>
-            <div  className='home-title'> Creating Moments</div>
-         </div>
+              <div ref={ref} className='home-titles'>
+                <div className='home-title-1'>
+                <motion.div initial="initial" variants={animation} animate={useInView ? 'enter' : ''} className='home-title first-title'>Brightening Spaces</motion.div>
+                </div>
+                <br className='gaps' />
+                <motion.div initial="initial" variants={animation} animate={useInView ? 'enter' : ''} className='home-title first-title'> Creating Moments</motion.div>
+              </div>
 
             <div className='home-description'>a world where lighting is an art form, a story telling <br className='gaps'/> medium, and a key player in sustainable design.</div>
             <motion.div whileHover={{scale:1.1}} className='home-explore-btn'>Explore Products</motion.div>
